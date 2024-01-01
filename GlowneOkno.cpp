7a1c5686ebead7a01fa7a3b8c8c7674d55391822
Mateusz Buczma <32811453+MBuczma@users.noreply.github.com>
2024-01-01 04:09:56 +0100

    przycisk z muzyką
    Działa

--- GlowneOkno.cpp
+++ GlowneOkno.cpp
@@ -1,5 +1,9 @@
 #include "GlowneOkno.h"
 #include "ui_GlowneOkno.h"
+#include <QMediaPlayer>
+#include <QFileInfo>
+#include <QAudioOutput>
+
 
 GlowneOkno::GlowneOkno(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +16,23 @@ GlowneOkno::~GlowneOkno()
 {
     delete ui;
 }
+
+void GlowneOkno::on_pushButton_clicked()
+{
+    QMediaPlayer* player = new QMediaPlayer;
+    QAudioOutput* audioOutput = new QAudioOutput;
+
+    audioOutput->setVolume(1.0); // Ustawienie głośności na 100%
+    player->setAudioOutput(audioOutput); // Powiązanie odtwarzacza z wyjściem audio
+    player->setSource(QUrl("qrc:/music/music/song.mp3")); // Zastąp odpowiednią ścieżką
+    player->play();
+    connect(player, &QMediaPlayer::errorOccurred, [](QMediaPlayer::Error error) {
+        qDebug() << "Błąd odtwarzania: " << error;
+    });
+    connect(player, &QMediaPlayer::playbackStateChanged, [](QMediaPlayer::PlaybackState state) {
+        if (state == QMediaPlayer::PlayingState) {
+            qDebug() << "Muzyka odtwarzana";
+        }
+    });
+}
+
